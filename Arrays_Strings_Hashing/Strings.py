dd419c014676f58d0c89335dc146dede96a63de5
Divyanshu Mehta <divyanshumehta1525@gmail.com>
2025-09-22 00:48:42 -0700

    added info about creating all substrings of a given string

--- Arrays_Strings_Hashing/Strings.py
+++ Arrays_Strings_Hashing/Strings.py
@@ -630,3 +630,79 @@ if __name__ == "__main__":
 
 
 # Generate all Sunstrings
+
+# [Expected Approach] - Using Iteration
+# Function to find all substrings
+def find_substrings(s):
+    
+    # to store all substrings
+    res = []
+    for i in range(len(s)):
+        for j in range(i, len(s)):
+            res.append(s[i:j+1])
+    return res
+
+s = 'abc'
+res = find_substrings(s)
+for i in res:
+    print(i, end=' ')
+    
+    
+# Output
+# a ab abc b bc c 
+
+
+
+# [Interesting Approach] - Using Recursion
+# Python program to find all the
+# substrings of given string
+
+# Recursive Function to find all
+# substrings of a string
+def subString(s, n, index, cur, res):
+
+    # if we have reached the
+    # end of the string
+    if index == n:
+        return
+
+    # add the character s[index]
+    # to the current string
+    cur += s[index]
+
+    # add the current string in result
+    res.append(cur)
+
+    # move to next index
+    subString(s, n, index + 1, cur, res)
+
+    # remove the current character
+    # from the current string
+    cur = cur[:-1]
+
+    # if current string is empty
+    # skip the current index to
+    # start the new substring
+    if not cur:
+        subString(s, n, index + 1, cur, res)
+
+# Function to find all substrings
+def findSubstrings(s):
+
+    # to store all substrings
+    res = []
+
+    # to store current string
+    cur = ""
+    subString(s, len(s), 0, cur, res)
+    return res
+
+if __name__ == "__main__":
+    s = "abc"
+    res = findSubstrings(s)
+    print(" ".join(res))
+    
+
+
+# Output
+# a ab abc b bc c 
