bacb8883b67415ca96bcabd96f9a3b1ac278d664
Divyanshu Mehta <divyanshumehta1525@gmail.com>
2025-11-24 04:08:04 -0800

    added Leetcode 125

--- Practice Questions/practice.py
+++ Practice Questions/practice.py
@@ -284,3 +284,70 @@
 
 # Time Complexity: O(n)
 # Space Complexity: O(1)
+
+
+
+
+
+
+
+
+
+
+# Leetcode 125
+
+# Valid Palindrome
+
+# A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+# Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+# Example 1:
+
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+# Example 2:
+
+# Input: s = "race a car"
+# Output: false
+# Explanation: "raceacar" is not a palindrome.
+# Example 3:
+
+# Input: s = " "
+# Output: true
+# Explanation: s is an empty string "" after removing non-alphanumeric characters.
+# Since an empty string reads the same forward and backward, it is a palindrome.
+
+
+
+# class Solution(object):
+#     def isPalindrome(self, s):
+#         """
+#         :type s: str
+#         :rtype: bool
+#         """
+#         s = s.lower()
+#         n = len(s)
+#         i = 0
+#         j = n-1
+
+#         while i < j:
+#             if s[i].isalnum() != True:
+#                 i+=1
+#                 continue
+#             if s[j].isalnum() != True:
+#                 j-=1
+#                 continue
+#             if s[i] == s[j]:
+#                 i+=1
+#                 j-=1
+#             else:
+#                 return False
+#         return True
+
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)       #due to using.lowercase() function.
