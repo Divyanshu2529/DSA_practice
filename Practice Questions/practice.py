dfe5547e1683999a13f81ba9a04ed3c0ab7f71ba
Divyanshu Mehta <divyanshumehta1525@gmail.com>
2025-11-26 18:23:50 -0800

    added Leetcode 344

--- Practice Questions/practice.py
+++ Practice Questions/practice.py
@@ -351,3 +351,45 @@
 
 # Time Complexity: O(n)
 # Space Complexity: O(n)       #due to using.lowercase() function.
+
+
+
+
+
+
+# Leetcode 344 - Reverse String
+
+# Write a function that reverses a string. The input string is given as an array of characters s.
+
+# You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+# Example 1:
+
+# Input: s = ["h","e","l","l","o"]
+# Output: ["o","l","l","e","h"]
+# Example 2:
+
+# Input: s = ["H","a","n","n","a","h"]
+# Output: ["h","a","n","n","a","H"]
+
+
+# class Solution(object):
+#     def reverseString(self, s):
+#         """
+#         :type s: List[str]
+#         :rtype: None Do not return anything, modify s in-place instead.
+#         """
+#         a=len(s)
+#         i=0
+#         j=a-1
+#         while i <= j:
+#             s[i], s[j] = s[j], s[i]
+#             i+=1 
+#             j-=1
+
+#         return s 
+        
+# Time Complexity: O(n)
+# Space Complexity: O(1) 
